ba4e8e1609547ccc8432531cac2a7672e4635951
Tsan Aung <myanmar@post.com>
2021-11-14 11:19:26 +0630

    Update data.js

--- biacms/support/api/data.js
+++ biacms/support/api/data.js
@@ -1,5 +1,6 @@
 fetch('https://springcondor.blogspot.com/feeds/posts/default?max-result=15&orderby=updated&alt=json')
 .then((res)=>res.json())
 .then((data)=>{
+  console.log(data.entry);
   document.getElementById("content").innerHTML = data.entry;
 })
